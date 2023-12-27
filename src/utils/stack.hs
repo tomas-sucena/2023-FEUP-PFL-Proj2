aba3448bf3134f2974d00b766f71babde012845e
tomas-sucena <tomas21sucena@gmail.com>
2023-12-27 00:03:34 +0000

    Add stack

---
+++ src/utils/stack.hs
@@ -0,0 +1,25 @@
+
+data Value =
+  I Integer | Tt | Ff
+
+instance Show Value where
+  show (I i) = show i
+  show Tt = show True
+  show Ff = show False
+
+type Stack = [Value]
+
+-- Pushes a value onto the stack.
+push :: Value -> Stack -> Stack
+push el [] = el:[]
+push el s = el:s
+
+-- Pops the value on top of the stack.
+pop :: Stack -> Stack
+pop [] = error "Run-time error"
+pop (x:xs) = xs
+
+-- Returns the value on top of the stack.
+top :: Stack -> Value
+top [] = error "Run-time error"
+top (x:xs) = x
