f89685fad6c57bfc1d0f1e9472abb3f4af1085b5
tomas-sucena <tomas21sucena@gmail.com>
2023-12-28 00:20:44 +0000

    Refactor value
    Replace 'Tt' and 'Ff' constants with a Bool.

--- src/Utils/Value.hs
+++ src/Utils/Value.hs
@@ -1,9 +1,8 @@
 module Utils.Value where
 
 data Value =
-  I Integer | Tt | Ff
+  I Integer | B Bool
 
 instance Show Value where
   show (I i) = show i
-  show Tt = show True
-  show Ff = show False
+  show (B b) = show b
